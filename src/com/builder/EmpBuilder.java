1cd0e24b8ebab247699af53f08c447c8b16b3252
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-24 02:06:08 +0530

    Added: part time Emp Wages

--- src/com/builder/EmpBuilder.java
+++ src/com/builder/EmpBuilder.java
@@ -2,23 +2,31 @@ package com.builder;
 
 public class EmpBuilder{
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Detail");
-		int IS_FULL_TIME = 1;
-		int EMP_RATE_PER_HOUR = 20;
-		int empHrs = 0;
-		int empWage = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME) {
-			empHrs = 8;
-			System.out.println("Employee is present");
-		} else {
-			empHrs = 0;
-			System.out.println("Employee is Abscent");
-
+		//constants
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
+		int EMP_RATE_PER_HR=20;
+		//variables
+		int sal;
+		int emp_Hrs;
+		//computation
+		double empCheck = Math.floor(Math.random() * 10)%3 ;
+		if(empCheck==IS_FULL_TIME)
+		{
+			emp_Hrs=8;
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage " + empWage);
+		else if(IS_PART_TIME==empCheck )
+		{
+			emp_Hrs=4;
+		}
+		else
+		{
+			emp_Hrs=0;
+		}
+			
+		    sal=(emp_Hrs*EMP_RATE_PER_HR);
+			System.out.println(sal);
+		}
+	
 
 	}
-
-}
