9e666c316272cadb8b97427ff6475c3c3fb0057d
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-24 02:36:15 +0530

    Added: Cal Wages for a Month

--- src/com/builder/EmpBuilder.java
+++ src/com/builder/EmpBuilder.java
@@ -1,27 +1,34 @@
 package com.builder;
 
-public class EmpBuilder{
-    public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
+public class EmpBuilder {
+
+	public static final int Is_Part_Time = 1;
+	public static final int Is_Full_Time = 2;
+	public static final int Emp_Rate_Per_Hrs = 20;
+	public static final int No_Of_Working_Days = 2;
+
 	public static void main(String[] args) {
-		//Variables
-		int empHrs = 0;
-		int empWage = 0;
-		//Computation
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-		case IS_PART_TIME:
-			empHrs = 4;
-			break;
-		case IS_FULL_TIME:
-			empHrs = 8;
-			break;
-	    default:
-	    	empHrs = 0;
+		int EmpWage = 0;
+		int Emphrs = 0;
+		int totalEmpWage = 0;
+		for (int days = 0; days < No_Of_Working_Days; days++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case Is_Part_Time:
+				Emphrs = 4;
+				break;
+			case Is_Full_Time:
+				Emphrs = 8;
+				break;
+			default:
+				Emphrs = 0;
+			}
+			EmpWage = Emphrs * Emp_Rate_Per_Hrs;
+			totalEmpWage += EmpWage;
+			System.out.println("Emp Wage" + " " + EmpWage);
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp wage: " + empWage);
+		System.out.println("totalEmpWage" + " " + totalEmpWage);
+
 	}
 
 }
