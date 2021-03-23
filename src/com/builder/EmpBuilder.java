979924c8ce3788c3a6226feef48c64ba86967a4b
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-24 00:43:54 +0530

    Added: Employee Attendance code

--- src/com/builder/EmpBuilder.java
+++ src/com/builder/EmpBuilder.java
@@ -1,7 +1,14 @@
 package com.builder;
+
 public class EmpBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Detail");
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is Abscent");
 	}
 
 }
