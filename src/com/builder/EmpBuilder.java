b93f8c29bb5adc27caacf6e7833bbceb52518a7f
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-24 02:45:28 +0530

    Calculate Wages till

--- src/com/builder/EmpBuilder.java
+++ src/com/builder/EmpBuilder.java
@@ -6,12 +6,13 @@ public class EmpBuilder {
 	public static final int Is_Full_Time = 2;
 	public static final int Emp_Rate_Per_Hrs = 20;
 	public static final int No_Of_Working_Days = 2;
+	public static final int Max_Hr_In_Month = 10;
 
 	public static void main(String[] args) {
-		int EmpWage = 0;
 		int Emphrs = 0;
-		int totalEmpWage = 0;
-		for (int days = 0; days < No_Of_Working_Days; days++) {
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalEmpHrs <= Max_Hr_In_Month && totalWorkingDays <= No_Of_Working_Days) {
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case Is_Part_Time:
@@ -23,12 +24,11 @@ public class EmpBuilder {
 			default:
 				Emphrs = 0;
 			}
-			EmpWage = Emphrs * Emp_Rate_Per_Hrs;
-			totalEmpWage += EmpWage;
-			System.out.println("Emp Wage" + " " + EmpWage);
+			totalEmpHrs += Emphrs;
+			System.out.println("Day : " + " " + totalWorkingDays + "Emphrs: " + Emphrs);
 		}
-		System.out.println("totalEmpWage" + " " + totalEmpWage);
-
+		int totalEmpWage = totalEmpHrs * Emp_Rate_Per_Hrs;
+		System.out.println("total Emp Wage" + " " + totalEmpWage);
 	}
 
 }
